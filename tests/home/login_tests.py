c99c2929630463c127cd1d46329bc3c0035844c2
sundararamankarthik <38950066+sundararamankarthik@users.noreply.github.com>
2018-08-19 12:37:22 +0530

    code clean up for login_tests.py
    Removed comments in login_tests.py

--- tests/home/login_tests.py
+++ tests/home/login_tests.py
@@ -17,19 +17,7 @@ class LoginTests(unittest.TestCase):
         driver.maximize_window()
         lp = LoginPage(driver)
         lp.login("test@email.com", "abcabc")
-        # Explicit Wait implementation
-        # wait = WebDriverWait(self.driver, 10, poll_frequency=1,ignored_exceptions=
-        #                                         [ElementNotVisibleException,
-        #                                          ElementNotSelectableException,
-        #                                         NoSuchElementException])
-        #
-        #
-        # element = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(@class,'navbar-link') and contains(text(),'Login')]")))
-
-        #Validate Login
-        #element = wait.until(EC.element_to_be_clickable((By.XPATH, "//img[@alt = 'test@email.com']")))
-        #element.click()
-
+        
 
 
     def test_logout(self):
